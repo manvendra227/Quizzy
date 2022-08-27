5f67d1367ec386a6d5311d6fa6726f00aa27a3c4
manvendra227 <manu227gkp@gmail.com>
2022-08-28 01:53:17 +0530

    test

--- app/src/main/java/com/example/quizzy/LoginActivity.kt
+++ app/src/main/java/com/example/quizzy/LoginActivity.kt
@@ -9,7 +9,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.layout_create_quiz)
 
     }
 }
