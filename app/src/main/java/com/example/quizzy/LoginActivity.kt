3a7d14929647df988dff0700b45dadc751dc7c1f
samarth4065 <samarth.1923ce1024@kiet.edu>
2022-08-28 18:28:58 +0530

    updated:create quiz

--- app/src/main/java/com/example/quizzy/LoginActivity.kt
+++ app/src/main/java/com/example/quizzy/LoginActivity.kt
@@ -9,7 +9,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.layout_create_quiz)
+        setContentView(R.layout.fragment_profile)
 
     }
 }
