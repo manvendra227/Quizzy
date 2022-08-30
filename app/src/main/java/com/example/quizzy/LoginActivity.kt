bda9940887c0135e1b210c50c0391ae7851dfaa2
manvendra227 <manu227gkp@gmail.com>
2022-08-30 22:10:10 +0530

    updated:create quiz layout

--- app/src/main/java/com/example/quizzy/LoginActivity.kt
+++ app/src/main/java/com/example/quizzy/LoginActivity.kt
@@ -1,10 +1,7 @@
 package com.example.quizzy
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
