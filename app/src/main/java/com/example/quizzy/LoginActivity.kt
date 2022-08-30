8e6032237970ab3e9a0e791b79d7121bdf8d49c9
samarth4065 <samarth.1923ce1024@kiet.edu>
2022-08-31 01:02:57 +0530

    t

--- app/src/main/java/com/example/quizzy/LoginActivity.kt
+++ app/src/main/java/com/example/quizzy/LoginActivity.kt
@@ -7,7 +7,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.layout_create_quiz)
+        setContentView(R.layout.activity_quiz_detail)
 
     }
 }
