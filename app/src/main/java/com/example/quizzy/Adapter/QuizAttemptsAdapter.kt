e24d8be8ca29ba6c60c774700d0a5d270091c543
manvendra227 <manu227gkp@gmail.com>
2022-09-29 02:35:59 +0530

    navigation in recycler items

--- app/src/main/java/com/example/quizzy/Adapter/QuizAttemptsAdapter.kt
+++ app/src/main/java/com/example/quizzy/Adapter/QuizAttemptsAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.quizzy.Adapter
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quizzy.R
+import com.example.quizzy.Screens.ProfileActivity
 import com.example.quizzy.Screens.QuizDetailActivity
 import com.example.quizzy.dataModel.model.AttemptModelQuiz
 import kotlinx.android.synthetic.main.list_item_quiz_attempts.view.*
@@ -16,24 +19,28 @@ import java.time.LocalDate
 import java.time.ZoneId
 import java.util.*
 
-class QuizAttemptsAdapter(private val passingScore:Double,private val activity: QuizDetailActivity): RecyclerView.Adapter<QuizAttemptsAdapter.MyViewHolder>() {
+class QuizAttemptsAdapter(
+    private val passingScore: Double,
+    private val activity: QuizDetailActivity
+) : RecyclerView.Adapter<QuizAttemptsAdapter.MyViewHolder>() {
 
 
     private var list: List<AttemptModelQuiz>? = null
 
     fun setAttemptList(list: List<AttemptModelQuiz>?) {
-        this.list=list
+        this.list = list
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_quiz_attempts, parent, false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_quiz_attempts, parent, false)
         return MyViewHolder(view)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.bind(list?.get(position)!!,passingScore,activity)
+        holder.bind(list?.get(position)!!, passingScore, activity)
     }
 
     override fun getItemCount(): Int {
@@ -41,12 +48,13 @@ class QuizAttemptsAdapter(private val passingScore:Double,private val activity: 
         else return list?.size!!
     }
 
-    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-        private val name=view.username
-        private val score=view.score
-        private val time=view.time
-        private val date=view.date
+        private val name = view.username
+        private val score = view.score
+        private val time = view.time
+        private val date = view.date
+        private val view = view.item
 
         @RequiresApi(Build.VERSION_CODES.O)
         fun bind(data: AttemptModelQuiz, passingScore: Double, activity: QuizDetailActivity) {
@@ -62,13 +70,24 @@ class QuizAttemptsAdapter(private val passingScore:Double,private val activity: 
             val month: Int = localDate.monthValue
             val day: Int = localDate.dayOfMonth
 
-            name.text=data.username
-            score.text=data.score.toString()
-            date.text="${day}/${month}/${year}"
-            time.text=myTime
-            if (data.score<passingScore){
+            name.text = data.username
+            score.text = data.score.toString()
+            date.text = "${day}/${month}/${year}"
+            time.text = myTime
+            if (data.score < passingScore) {
                 score.setTextColor(Color.parseColor("#FF453A"))
             }
+
+            view.setOnClickListener {
+                if (!data.userId.isNullOrEmpty()) {
+                    val intent = Intent(activity, ProfileActivity::class.java)
+                    intent.putExtra("userId", data.userId)
+                    activity.startActivity(intent)
+                } else {
+                    Toast.makeText(activity, "No User Found", Toast.LENGTH_SHORT).show()
+                }
+            }
+
 
         }
 
--- app/src/main/java/com/example/quizzy/Adapter/UserAttemptsAdapter.kt
+++ app/src/main/java/com/example/quizzy/Adapter/UserAttemptsAdapter.kt
@@ -1,37 +1,43 @@
 package com.example.quizzy.Adapter
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quizzy.R
 import com.example.quizzy.Screens.ProfileActivity
+import com.example.quizzy.Screens.QuizDetailActivity
 import com.example.quizzy.dataModel.model.AttemptModelUser
+import com.example.quizzy.utilities.MyToast
 import kotlinx.android.synthetic.main.list_item_user_attempts.view.*
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.ZoneId
 import java.util.*
 
-class UserAttemptsAdapter(private val activity: ProfileActivity): RecyclerView.Adapter<UserAttemptsAdapter.MyViewHolder>() {
+class UserAttemptsAdapter(private val activity: ProfileActivity) :
+    RecyclerView.Adapter<UserAttemptsAdapter.MyViewHolder>() {
 
     private var list: List<AttemptModelUser>? = null
 
     fun setAttemptList(list: List<AttemptModelUser>?) {
-        this.list=list
+        this.list = list
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_user_attempts, parent, false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_user_attempts, parent, false)
         return MyViewHolder(view)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.bind(list?.get(position)!!,activity)
+        holder.bind(list?.get(position)!!, activity)
     }
 
     override fun getItemCount(): Int {
@@ -39,12 +45,13 @@ class UserAttemptsAdapter(private val activity: ProfileActivity): RecyclerView.A
         else list?.size!!
     }
 
-    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-        private val name=view.quizname
-        private val score=view.score
-        private val time=view.time
-        private val date=view.date
+        private val name = view.quizname
+        private val score = view.score
+        private val time = view.time
+        private val date = view.date
+        private val view = view.view
 
         @RequiresApi(Build.VERSION_CODES.O)
         fun bind(data: AttemptModelUser, activity: ProfileActivity) {
@@ -60,17 +67,27 @@ class UserAttemptsAdapter(private val activity: ProfileActivity): RecyclerView.A
             val month: Int = localDate.monthValue
             val day: Int = localDate.dayOfMonth
 
-            name.text=data.title
-            score.text=data.score.toString()
-            date.text="${day}/${month}/${year}"
-            time.text=myTime
+            name.text = data.title
+            score.text = data.score.toString()
+            date.text = "${day}/${month}/${year}"
+            time.text = myTime
 
-            if (data.score<data.passingScore){
+            if (data.score < data.passingScore) {
                 score.setTextColor(Color.parseColor("#FF453A"))
+            }
+
+            view.setOnClickListener {
+                if (!data.quizID.isNullOrEmpty()) {
+                    val intent = Intent(activity, QuizDetailActivity::class.java)
+                    intent.putExtra("quizId", data.quizID)
+                    activity.startActivity(intent)
+                }else{
+                    Toast.makeText(activity,"No Quiz Found",Toast.LENGTH_SHORT).show()
+                }
             }
         }
 
-        fun resetViews(){
+        fun resetViews() {
             score.setTextColor(Color.parseColor("#30D158"))
         }
 
